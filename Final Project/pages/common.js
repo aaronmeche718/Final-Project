0ce0183e719bb6ccc54fc9dd19b14e3640ea2ed1
aaronmeche718 <40175874+aaronmeche718@users.noreply.github.com>
2018-06-27 14:11:17 -0500

    Add files via upload

--- Final Project/pages/common.js
+++ Final Project/pages/common.js
@@ -1,22 +1,15 @@
-$(".city").click(function () {
-    //api call here
-    var city = $(".city").val();
-    var uri = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",ru$units=si&appid=4e651d58543d2bad33b52678226ee6e6";
-    console.log (uri);
-    $.get(uri, onRes);
+$(document).ready(function(){
+    $(".city").click(function() {
+        var city = $(".city").val();
+        var uri = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",ru$units=si&appid=380be86ab068031579b0509d4f27df73";
+        console.log(uri);
+        $.get(uri, onRes);
+    });
 });
-
 function onRes(response) {
     console.log(response);
     $(".temp").html(response.main.temp);
     $(".city-name").html(response.name);
     $(".weather").html(response.weather[0].description);
-}
-function myMap() {
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.12),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-    }
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    console.log(response.main.temp);
 }
